ee836e2424e2870cb01308caeb015d91de7d30d7
Jimmy <jmiller87@gmail.com>
2022-08-12 21:46:03 -0400

    few actor display updates

--- src/PerformsActions.php
+++ src/PerformsActions.php
@@ -16,10 +16,12 @@ trait PerformsActions
     public function logAction(string $action, array $tags = [], array $context = [])
     {
         return AuditLogger::make(
-            (new Actor)->setIdentifier(
-                $this->getAuditIdentifierKey(), $this->getAuditIdentifierValue()
-            ))
-            ->setAction($action)
+            (new Actor)
+                ->setIdentifier(
+                    $this->getAuditIdentifierKey(), $this->getAuditIdentifierValue()
+                )
+                ->setDisplay($this->AuditDisplay())
+        )->setAction($action)
             ->setTags($tags)
             ->setContext($context)
             ->save();
@@ -31,6 +33,19 @@ trait PerformsActions
     protected function getAuditIdentifierKey()
     {
         return Str::snake(strtolower(config('app.name'))) . '_id';
+    }
+
+    /**
+     * @return string
+     */
+    protected function getAuditDisplayValue()
+    {
+
+        if (method_exists($this, '__toString')) {
+            return $this->__toString();
+        }
+
+        return $this->getAuditIdentifier();
     }
 
     /**
